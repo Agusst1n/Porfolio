35d457f9d267ffb6df59e47f3733c4a7fd7e404b
Agustín <88418234+Agusst1n@users.noreply.github.com>
2021-12-28 01:33:20 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ let menuToggle = document.querySelector('.menuToggle')
 let body = document.querySelector('body')
 let navigation = document.querySelector('.navigation')
 
+load();
+
 dayNight.onclick = function () {
     body.classList.toggle('dark')
     dayNight.classList.toggle('active')
+
+    store(body.classList.contains('dark'));
 }
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('active')
     navigation.classList.toggle('active')
+}
+
+
+function load(){
+    const dark = localStorage.getItem('dark')
+
+    if(!dark){
+        store('false');
+    }else if (dark == 'true'){
+        body.classList.add('dark')
+    }
+}
+function store(value){
+    localStorage.setItem('dark', value);
 }
